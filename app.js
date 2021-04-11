1a7cd3a62cbbdac37520b7e2b2211c94603bba19
Johar Ali <50805982+JoharPiaic@users.noreply.github.com>
2021-04-11 23:58:37 +0500

    Java class practice Code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-alert( "Hello World" );
-alert("Error Here Block it");
-var nam = "Welcome Johar";
-alert (nam);
+// alert( "Hello World" );
+// alert("Error Here Block it");
+var name = prompt(" Your Name")
+alert ("Your name is " + name);
